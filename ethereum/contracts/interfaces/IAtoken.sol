c5051cade9ca584b21ad04c7d9cd1dfa5a663b14
Viraz <virajm72@gmail.com>
2020-05-27 09:14:20 +0530

    Final Contract Changes

--- ethereum/contracts/interfaces/IAtoken.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.6.0;
-
-interface ATokenInterface {
-     function redirectInterestStream(address _to) external;
-     function balanceOf(address _user) external;
-     function redeem(uint256 _amount) external;
-}
